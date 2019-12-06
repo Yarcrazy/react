69a0cf0f947f0cb5e27c14789b965e12bf77802d
Yarcrazy <yacrazy@yandex.ru>
2019-12-06 23:24:55 +0700

    build 1

---
+++ build/precache-manifest.1f22326dfacbfd7ebedf974cb5d5d462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e467b5b912f07f5e948e9e576e9105b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef9c28498fd8fb8e6eef",
+    "url": "/static/css/main.30185390.chunk.css"
+  },
+  {
+    "revision": "0b79465b4bf2026d5671",
+    "url": "/static/js/2.e2d6ed90.chunk.js"
+  },
+  {
+    "revision": "ef9c28498fd8fb8e6eef",
+    "url": "/static/js/main.ad63a383.chunk.js"
+  },
+  {
+    "revision": "94ef188d047bb3ac1159",
+    "url": "/static/js/runtime-main.4d26af1b.js"
+  }
+]);
