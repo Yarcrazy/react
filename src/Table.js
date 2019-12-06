d3c755a1f76bb299e3e92df69b1e7e46e1586b18
Yarcrazy <yacrazy@yandex.ru>
2019-12-06 23:16:37 +0700

    1

---
+++ src/Table.js
@@ -0,0 +1,14 @@
+import * as React from "react/cjs/react.development";
+
+class Table extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Table;
