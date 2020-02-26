a3b20a09bd350ced3fbc930db79d8072adb22c4b
Yarcrazy <yacrazy@yandex.ru>
2020-02-26 22:09:57 +0700

    th

--- src/TestTable.js
+++ src/TestTable.js
@@ -4,20 +4,21 @@ import THead from "./Table/THead";
 import TBody from "./Table/TBody";
 import Tr from "./Table/Tr";
 import Td from "./Table/Td";
+import Th from "./Table/Th";
 
 export default function TestTable() {
   return (
     <Table>
       <THead className={'fixed'}>
         <Tr>
-          <Td className={'col-fixed'}>1</Td>
-          <Td>1</Td>
-          <Td>2</Td>
-          <Td className={'col-fixed'}>3</Td>
-          <Td>3</Td>
-          <Td>3</Td>
-          <Td>3</Td>
-          <Td>3</Td>
+          <Th className={'col-fixed'}>1</Th>
+          <Th>1</Th>
+          <Th>2</Th>
+          <Th className={'col-fixed'}>3</Th>
+          <Th>3</Th>
+          <Th>3</Th>
+          <Th>3</Th>
+          <Th>3</Th>
         </Tr>
       </THead>
       <TBody>
